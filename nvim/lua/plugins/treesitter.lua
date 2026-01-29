fefe7b7e57f326df02965d1611d052f84b6437c4
Santeri <kaisanteri@proton.me>
2026-01-29 16:09:55 +0200

    Update treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  lazy = false,
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "cpp", "rust", "python", "zig", "go", "html", "css", "svelte", "javascript", "typescript", "lua", "markdown" , "tsx", "elixir" },
-      highlight = {
-        enable = true,
-      },
-    })
+    require("nvim-treesitter").install{
+      "c", "cpp", "rust", "python", "zig", "go", "html", "css", "svelte", "javascript", "typescript", "lua", "markdown" , "tsx", "elixir" 
+    }
   end
 }
 
